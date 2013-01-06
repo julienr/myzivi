051ccd3e954fb23703fe79321d08aa0efcc0dfed
Julien Rebetez <julien@fhtagn.net>
2013-01-06 21:39:54 +0100

    Use marker clusters and marker spiderer

--- ziviweb/settings.py
+++ ziviweb/settings.py
@@ -1,3 +1,4 @@
+import os
 # Django settings for ziviweb project.
 
 DEBUG = True
@@ -56,7 +57,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+#STATIC_ROOT = ''
+STATIC_ROOT = os.path.dirname(os.path.normpath(__file__)) + '/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
