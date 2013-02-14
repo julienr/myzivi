93085e0e0a0755003b81b6fedfa36cebd9ef4ac1
Julien Rebetez <julien.rebetez@gmail.com>
2013-02-14 14:59:18 +0100

    Shell scripts update

--- reimport.sh
+++ reimport.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-DDIR=$PWD/_data/scraped_`date +%F-%T`
-mkdir -p $DDIR
+# execute postprocess and import_items using given _data directory
+DDIR=$PWD/$1
+if [ ! -d "$DDIR" ]; then
+	echo "Directory doesn't exist : $DDIR"
+	exit 1
+fi
 export SCRAPY_DATA_DIR=$DDIR
 export PYTHONPATH=.:/home/myzivi/django-myzivi:$PYTHONPATH
 export DJANGO_SETTINGS_MODULE=ziviweb.settings_deployment
-# Crawler
-scrapy crawl zivi
 # Postprocess (geotagging, translation, ...)
 python ziviscrap/postprocess.py
 # Insert into DB
 python zivimap/import_items.py
-# Delete old scraped directory
-python rotate_data.py
