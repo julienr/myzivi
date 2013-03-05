5ef8b5e547ef8d1031e579fcc5957bdb3552f92f
Julien Rebetez <julien@fhtagn.net>
2013-03-05 13:58:55 +0100

    Enable GZIP compression

--- ziviweb/settings.py
+++ ziviweb/settings.py
@@ -111,6 +111,7 @@ TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
